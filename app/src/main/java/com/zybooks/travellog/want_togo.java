68340dcd58e281625ca3fe5d91a9df250b258192
Gabri-ella-1 <ellabella0110@gmail.com>
2023-07-23 14:24:25 -0400

    Finished the nav and fragments

---
+++ app/src/main/java/com/zybooks/travellog/want_togo.java
@@ -0,0 +1,16 @@
+package com.zybooks.travellog;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class want_togo extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.want_togo, container, false);
+    }
+}
